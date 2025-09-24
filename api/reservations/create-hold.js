7fe2741b39ac1ebd0b2753574c18c98e3818b19c
Stefp799 <piardis@gmail.com>
2025-09-23 20:21:01 -0400

    Fix Vercel function - convert ES modules to CommonJS

--- api/reservations/create-hold.js
+++ api/reservations/create-hold.js
@@ -1,7 +1,7 @@
-import Stripe from 'stripe'
-import nodemailer from 'nodemailer'
-
 // Initialize Stripe
+const Stripe = require('stripe')
+const nodemailer = require('nodemailer')
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // Initialize Email Transporter (Gmail SMTP)
@@ -75,7 +75,7 @@ async function sendEmail(to, subject, html) {
   }
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' })
   }
